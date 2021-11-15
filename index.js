7f3aaa10bb68d9b506e68ae47ad027d206ef2a33
Elena Zdanovska <zdanovskaolena@gmail.com>
2021-11-15 14:21:03 +0200

    Add Counter

---
+++ index.js
@@ -0,0 +1,36 @@
+class Counter extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            counter : 0,
+        }
+    }
+    increment = ()=>{
+        this.setState({
+            counter: this.state.counter+1,
+        })
+    }
+    decrement = ()=>{
+        const {counter} = this.state;
+        if(counter>0){
+            this.setState({
+                counter: this.state.counter-1,
+            })        
+        }
+    }
+    render(){
+        const {counter} = this.state;
+        return  React.createElement(React.Fragment, 
+            null,
+            React.createElement('h1', null, counter),
+            React.createElement('button', {
+                onClick: this.decrement
+            }, '-'),
+            React.createElement('button', {
+                onClick: this.increment
+            }, '+')
+        )
+    }
+}
+const reactElement = React.createElement(Counter);
+ReactDOM.render(reactElement, document.getElementById('root'));
